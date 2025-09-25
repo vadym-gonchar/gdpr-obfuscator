358f59662accc0420350f4291c31528fd4575553
Vadym Honchar <vadym.gonchar@gmail.com>
2025-09-25 13:50:34 +0100

    change s3 bucket name

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     region = "eu-west-2"
-    bucket = "de-gdpr-obfuscator-terraform-statefiles" # REPLACE with your bucket
+    bucket = "gdpr-obfuscator-statefiles" # REPLACE with your bucket
     key    = "extract-statefile"
   }
 }
