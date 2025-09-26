bf7da853db3de8ea0109cdbb52e5da9740014324
Vadym Honchar <vadym.gonchar@gmail.com>
2025-09-26 16:19:04 +0100

    add config for constants

---
+++ src/config.py
@@ -0,0 +1,7 @@
+"""
+Shared configuration constants for the GDPR Obfuscator tool.
+"""
+
+MAX_FILE_SIZE = 1_000_000
+SUPPORTED_FORMATS = ['csv', 'json', 'parquet']
+SUPPORTED_STRATEGIES = ['mask', 'hash']
