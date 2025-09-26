46a7ec59f6470180acab6163c5bcb71f8e2a2f10
Vadym Honchar <vadym.gonchar@gmail.com>
2025-09-26 16:03:49 +0100

    fix: Parameters are now nested under the "Input" key by the Step Function

---
+++ src/lambda_handler.py
@@ -0,0 +1,26 @@
+import logging
+import json
+from s3_utils import obfuscate_data
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def lambda_handler(event, context):
+    logger.info(f"Received event: {json.dumps(event)}")
+
+    # Let Lambda's runtime handle exceptions for better error reporting in CloudWatch
+    try:
+        # Parameters are now nested under the "Input" key by the Step Function
+        params = event.get("Input", event)
+        result = obfuscate_data(params, return_bytes=False)
+        logger.info("File obfuscated successfully")
+        # Return a flat structure, which is easier for Step Functions to process
+        return result
+
+    except ValueError as e:
+        logger.error(f"A validation error occurred: {str(e)}")
+        raise  # Re-raise the exception to let Lambda runtime handle it
+
+    except Exception as e:
+        logger.exception("Unexpected error in lambda_handler")
+        raise  # Re-raise for detailed error logging
