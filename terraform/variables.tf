8059d975301e31aae2baca55d68782ed6ae23293
Vadym Honchar <vadym.gonchar@gmail.com>
2025-09-29 22:01:24 +0100

    add comment

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,23 +1,25 @@
+# Terraform variables for the GDPR Obfuscator project
+# Lambda function name
 variable "lambda_name" {
   type    = string
   default = "obfuscator_tool"
 }
-
+# S3 bucket name
 variable "s3_bucket_name" {
   type    = string
   default = "gdpr-ingestion-bucket"
 }
-
+# Step Function name
 variable "step_function_name" {
   type    = string
   default = "InvokeLambdaAndRetrieveFile"
 }
-
+# Log retention period in days for CloudWatch logs
 variable "log_retention_days" {
   type    = number
   default = 14
 }
-
+# Memory size in MB for the Lambda function
 variable "lambda_memory_size" {
   type    = number
   default = 512
